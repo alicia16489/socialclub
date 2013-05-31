cca140ecd26d8201b40ebf9e8c32b91f3bcf1f87
benoit ciret <benoit.ciret@supinternet.fr>
2013-05-31 04:50:54 +0200

    reset pass

--- js/coucou.js
+++ js/coucou.js
@@ -1,7 +1,7 @@
 $('.toggle_box').click(function(){
 
 	if($(this).parent().height() == 40) {
-		$(this).parent().animate({height:parentHeight});
+		$(this).parent().animate({height:'300px'});
 		$(this).children().attr('class', 'triangle_up_black');
 	} else {
 		$(this).parent().animate({height:'40px'});
